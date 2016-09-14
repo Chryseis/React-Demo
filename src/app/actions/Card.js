55a513f5c6dee0487472169454dc832380eb6d60
Chrys <294931812@qq.com>
2016-09-14 16:55:27 +0800

    redux

---
+++ src/app/actions/Card.js
@@ -0,0 +1,3 @@
+/**
+ * Created by chrys_000 on 2016/9/14.
+ */
---
+++ src/app/constants/ActionType.js
@@ -0,0 +1,6 @@
+/**
+ * Created by chrys_000 on 2016/9/14.
+ */
+export const ADD_TODO='ADD_TODO';
+export const DELETE_TODO='DELETE_TODO';
+export const EDIT_TODO='EDIT_TODO';
