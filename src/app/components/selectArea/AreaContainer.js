ca49711b62d313b3263ec231909f466793a2dafc
AllenFeng <allenfeng@mintcode.com>
2016-12-29 18:11:55 +0800

    selectarea

--- src/app/components/selectArea/AreaContainer.js
+++ src/app/components/selectArea/AreaContainer.js
@@ -6,7 +6,6 @@
 export default class AreaContainer extends React.Component {
     render() {
         const {item,getCell,show,currentItem}=this.props;
-        console.log(currentItem);
         return <div className={classnames({container:true,hide:show})}>
             {item && item.length > 0 && item.map((child, i)=>(<a href="javascript:void(0)" className={classnames({active:child.name==currentItem})} key={i} onClick={getCell&&getCell.bind(null,child.cell,child.name)}>{child.name}</a>))}
         </div>
